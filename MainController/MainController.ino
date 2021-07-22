bc80012ed0dc3c837c359549600fbd43607c8185
James <jdefowler@gmail.com>
2021-07-22 11:01:12 -0700

    Implemented temperature regulation

--- MainController/MainController.ino
+++ MainController/MainController.ino
@@ -1,9 +1,11 @@
 #include "DHT.h"
+#include <Servo.h>                                                          //new line
 #define DHTPIN 2
 #define DHTTYPE DHT11
 
 
 DHT dht(DHTPIN, DHTTYPE);
+Servo myservo;                                                              
 
 
 int pinSensor = 2;
@@ -11,10 +13,14 @@ int pinHum = 5;
 int in1 = 10;
 int in2 = 11;
 int pinFan = 6; 
+int pinservo = 9;                                                           
+int servopos0 =55; //vent closed                                            
+int servopos1 = 0;  //vent open                                             
+
 
 float temperatureGoal = 0;
 float relativeHGoal = 0; 
-boolean startup = true;
+bool startup = true;
 
 const String comtemplate ="000/00.00/000" ;// hum/temp/pwm 
 const int sizeOfCom = comtemplate.length();
@@ -24,7 +30,7 @@ float currentTemp = 0;
 float currentH = 0;
 int pwm = 0;
 void setup() {
-  // put your setup code here, to run once:
+  
   Serial.begin(9600);
   dht.begin(); // initialize the sensor
 
@@ -33,6 +39,8 @@ void setup() {
   pinMode(in1, OUTPUT);
   pinMode(in2,OUTPUT);
   pinMode(13,OUTPUT);
+
+  myservo.attach(pinservo);                                                  //new line
   
 }
 
@@ -46,8 +54,8 @@ void loop() {
       getParameters();
       startup = false;
       delay(500);
-
   }
+  
   if(startup == false){
    if(currentH !=  relativeHGoal){
     if(currentH < relativeHGoal){
@@ -65,13 +73,27 @@ void loop() {
     
   }
 
+
+  if(startup == false){                                             //New lines
+   if(currentTemp !=  temperatureGoal){
+    if(currentTemp < temperatureGoal){
+      myservo.write(servopos0);
+      delay(500);
+      
+    }else if(currentTemp > temperatureGoal){
+      myservo.write(servopos1);
+      delay(500);
+    }
+   }
+    
+  }                                                                 // Till Here
+
 delay(1000);
 
 }
 void parseReadings(String str){
  if(str.charAt(3)=='/' && str.charAt(6) == '.' && str.charAt(9) == '/'){
    relativeHGoal =  str.substring(0,3).toFloat();
-   //Serial.println(relativeHGoal);
    
    temperatureGoal = str.substring(4,9).toFloat();
   // Serial.println(temperatureGoal);
@@ -86,12 +108,9 @@ Serial.print("/" );
 Serial.print(currentTemp);
 Serial.print("/");
 Serial.println(pwm);
-
-
-  
 }
 
- void getReadings(){
+void getReadings(){
     // read humidity
   currentH  = dht.readHumidity();
   // read temperature as Celsius
@@ -99,9 +118,7 @@ Serial.println(pwm);
    Serial.print("Humidity: ");
     Serial.print(currentH);
     Serial.print("%");
-
     Serial.print("  |  "); 
-
     Serial.print("Temperature: ");
     Serial.print(currentTemp);
     Serial.println("°C ~ ");
